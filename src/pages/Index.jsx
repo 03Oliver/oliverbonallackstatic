9aaeeaacf782ca8da5cc9f919eab8efdb34e4244
03Oliver <109152422+03Oliver@users.noreply.github.com>
2024-06-22 13:11:40 +0100

    fixes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -28,58 +28,58 @@ const Index = () => {
   return (
     <Container centerContent maxW="100vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgGradient="linear(to-r, white, lightblue)" color="black" fontFamily="Roboto, sans-serif" overflow="hidden">
       <Box textAlign="center" mb={8}>
-        <Box as="pre" fontSize="2xl" whiteSpace="nowrap" overflow="hidden" borderRight="2px solid" animation={`${typing} 4s steps(${fullText.length}), ${blink} 0.75s step-end infinite`}>
+        <Box as="pre" fontSize="3xl" fontWeight="bold" whiteSpace="nowrap" overflow="hidden" borderRight="2px solid" animation={`${typing} 4s steps(${fullText.length}), ${blink} 0.75s step-end infinite`}>
           {text}
         </Box>
       </Box>
       <Flex justify="center" align="center" width="100%">
         <SimpleGrid columns={{ base: 2, md: 3 }} spacing={8} textAlign="center">
           <Link href="https://www.linkedin.com/in/oliverbonallack" isExternal>
-            <Box>
+            <Box textAlign="center">
               <Box as={FaLinkedin} size="48px" _hover={{ transform: "scale(1.1)" }} transition="transform 0.2s" />
-              <Text mt={0.5}>LinkedIn</Text>
+              <Text mt={0.5} fontFamily="Roboto, sans-serif" fontWeight="bold">LinkedIn</Text>
             </Box>
           </Link>
 
           <Link href="https://www.youtube.com/@collectivevc" isExternal>
-            <Box>
+            <Box textAlign="center">
               <Box as={FaYoutube} size="48px" _hover={{ transform: "scale(1.1)" }} transition="transform 0.2s" />
-              <Text mt={0.5}>YouTube</Text>
+              <Text mt={0.5} fontFamily="Roboto, sans-serif" fontWeight="bold">YouTube</Text>
             </Box>
           </Link>
 
           <Link href="https://collectivevc.substack.com" isExternal>
-            <Box>
+            <Box textAlign="center">
               <Box as={FaEnvelope} size="48px" _hover={{ transform: "scale(1.1)" }} transition="transform 0.2s" />
-              <Text mt={0.5}>Substack</Text>
+              <Text mt={0.5} fontFamily="Roboto, sans-serif" fontWeight="bold">Substack</Text>
             </Box>
           </Link>
 
           <Link href="tel:+447572794778" isExternal>
-            <Box>
+            <Box textAlign="center">
               <Box as={FaWhatsapp} size="48px" _hover={{ transform: "scale(1.1)" }} transition="transform 0.2s" />
-              <Text mt={0.5}>WhatsApp</Text>
+              <Text mt={0.5} fontFamily="Roboto, sans-serif" fontWeight="bold">WhatsApp</Text>
             </Box>
           </Link>
 
           <Link href="https://www.oliverbonallack.com" isExternal>
-            <Box>
+            <Box textAlign="center">
               <Box as={FaFilePdf} size="48px" _hover={{ transform: "scale(1.1)" }} transition="transform 0.2s" />
-              <Text mt={0.5}>CV/Resume</Text>
+              <Text mt={0.5} fontFamily="Roboto, sans-serif" fontWeight="bold">CV</Text>
             </Box>
           </Link>
 
           <Link href="https://www.oliverbonallack.com" isExternal>
-            <Box>
+            <Box textAlign="center">
               <Box as={FaFilePdf} size="48px" _hover={{ transform: "scale(1.1)" }} transition="transform 0.2s" />
-              <Text mt={0.5}>Portfolio Deck</Text>
+              <Text mt={0.5} fontFamily="Roboto, sans-serif" fontWeight="bold">Portfolio Deck</Text>
             </Box>
           </Link>
 
           <Link href="https://www.calendly.com/oliverbonallack" isExternal>
-            <Box>
+            <Box textAlign="center">
               <Box as={FaCalendar} size="48px" _hover={{ transform: "scale(1.1)" }} transition="transform 0.2s" />
-              <Text mt={0.5}>Calendly</Text>
+              <Text mt={0.5} fontFamily="Roboto, sans-serif" fontWeight="bold">Calendly</Text>
             </Box>
           </Link>
         </SimpleGrid>
