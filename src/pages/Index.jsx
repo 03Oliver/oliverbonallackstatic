c53f9d57c4e58dd7ed609e108e8a558075cfe203
03Oliver <109152422+03Oliver@users.noreply.github.com>
2024-06-22 12:51:02 +0100

    labels, colour

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -26,7 +26,7 @@ const Index = () => {
   }, [fullText]);
 
   return (
-    <Container centerContent maxW="100vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgGradient="linear(to-r, black, gray.800)" color="white" fontFamily="Roboto, sans-serif" overflow="hidden">
+    <Container centerContent maxW="100vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgGradient="linear(to-r, white, gray.800)" color="black" fontFamily="Roboto, sans-serif" overflow="hidden">
       <Box textAlign="center" mb={8}>
         <Box as="pre" fontSize="2xl" whiteSpace="nowrap" overflow="hidden" borderRight="2px solid" animation={`${typing} 4s steps(${fullText.length}), ${blink} 0.75s step-end infinite`}>
           {text}
